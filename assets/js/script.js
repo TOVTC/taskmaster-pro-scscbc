8fc411b8212092d8d537cae321d4b086309a098e
TOVTC <veronica.tc.to@outlook.com>
2022-03-23 15:20:38 -0400

    added drag and drop functionality for both desktop and mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,4 +165,63 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+//make columns sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function() {
+    //array to store the task data in
+    var tempArr = [];
+    //loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      //add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    //trim down list's Id to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "")
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    console.log(tempArr);
+    saveTasks();
+  }
+});
 
+//make the trash area droppable
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event) {
+    console.log("out");
+  }
+})
